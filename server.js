d2a30bf605adaa62b5590dd80734e4fdfee990f0
Luong hoan <luonghoan1862007@gmail.com>
2025-09-10 15:39:41 +0700

    update

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const post = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(express.static('public'))
 
 app.post('/', (req, res) => {
   console.log(`Message from hello is ${req.body.hello}`);
