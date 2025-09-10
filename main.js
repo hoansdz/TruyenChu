189d831a6bd53b427d7331a87947b1beceb770c4
Luong hoan <luonghoan1862007@gmail.com>
2025-09-10 13:41:05 +0700

    update

---
+++ main.js
@@ -0,0 +1,35 @@
+function addStory(parent, story) {
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    const title = document.createElement('p');
+    const evaluate = document.createElement('p');
+    title.innerHTML = story.title;
+    img.src = story.image;
+    evaluate.innerHTML = `⭐ ${story.star} / 10`;
+    div.appendChild(img);
+    div.appendChild(title);
+    div.appendChild(evaluate);
+    parent.appendChild(div);
+}
+
+function init() {
+    const grid = document.getElementById('suggest-grid');
+    const stories = [
+        {
+            image: 'images/i1.jpg',
+            title: 'Nhất thế phi thiên',
+            star: 8,
+        }, 
+        {
+            image: 'images/i2.jpg',
+            title: 'Vợ Nhỏ Yêu Nghiệt Của Âu Thiếu - Đàm Tiểu Ân',
+            star: 9,
+        }
+    ];
+    if (stories.length) {
+        grid.getElementsByClassName('empty')[0].style.display = 'none';
+    }
+    for (const story of stories) {
+        addStory(grid, story);
+    }
+}
