b1ce985adbf7b1b939f02e17b60658316baf9aed
Luong hoan <luonghoan1862007@gmail.com>
2025-09-11 23:04:04 +0700

    update

--- public/backend/db.js
+++ public/backend/db.js
@@ -1,7 +1,10 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: 'postgresql://truyenchu_user:iQGg2qtFZmCNpKaA1E2tb3pMQoNZuLtI@dpg-d30ij6vdiees73fvm34g-a.oregon-postgres.render.com/truyenchu'
+  connectionString: 'postgresql://truyenchu_user:iQGg2qtFZmCNpKaA1E2tb3pMQoNZuLtI@dpg-d30ij6vdiees73fvm34g-a.oregon-postgres.render.com/truyenchu',
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
